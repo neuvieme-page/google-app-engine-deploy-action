a146690b27550ce4d80cb05d3edb9a83ef07c5a8
Thomas Démocrite <thomas.democrite@pm.me>
2020-06-22 12:11:48 +0200

    feat(basic action): google cloud app engine delpoy action

---
+++ index.js
@@ -0,0 +1,46 @@
+const core = require("@actions/core");
+const fs = require("fs");
+const childProcess = require("child_process");
+
+const childProcessOptions = { stdio: "inherit" };
+
+function processServiceAccount(serviceAccount, serviceAccountFile) {
+  core.startGroup("Process Service Account");
+  fs.writeFileSync(serviceAccountFile, serviceAccount);
+  childProcess.execSync(
+    `gcloud auth activate-service-account --key-file ${serviceAccountFile}`,
+    childProcessOptions
+  );
+  core.endGroup();
+}
+
+function setGoogleCloudProject(projectId) {
+  core.startGroup("Set Google Cloud Project");
+  childProcess.execSync(
+    `gcloud config set project ${projectId}`,
+    childProcessOptions
+  );
+  core.endGroup();
+}
+
+function deployToGoogleCloudAppEngine() {
+  core.startGroup("Deploy to Google Cloud App Engine");
+  childProcess.execSync("gcloud app deploy", childProcessOptions);
+  core.endGroup();
+}
+
+function unlinkServiceAccountFile(serviceAccountFile) {
+  core.startGroup("Unlink Service Account File");
+  fs.unlinkSync(serviceAccountFile);
+  core.endGroup();
+}
+
+try {
+  const serviceAccountFile = `tmp/${new Date().getTime()}.json`;
+  processServiceAccount(core.getInput("service_account"), serviceAccountFile);
+  setGoogleCloudProject(core.getInput("project_id"));
+  deployToGoogleCloudAppEngine();
+  unlinkServiceAccountFile(serviceAccountFile);
+} catch (error) {
+  core.setFailed(error.message);
+}
