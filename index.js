35251e42053ac16c24f53fccfeaa06fcd8ff479d
Thomas Démocrite <thomas.democrite@pm.me>
2020-06-26 11:46:57 +0200

    revert: on verra plus tard pour les erreurs

--- index.js
+++ index.js
@@ -2,63 +2,37 @@ const core = require("@actions/core");
 const fs = require("fs");
 const childProcess = require("child_process");
 
-const childProcessOptions = { stdio: "inherit", encoding: "utf-8" };
-
-function handleError(childProcess) {
-  if (childProcess.error || childProcess.stderr.toString().trim())
-    throw new Error(childProcess.error);
-}
+const childProcessOptions = { stdio: "inherit" };
 
 function processServiceAccount(serviceAccount, serviceAccountFile) {
   core.startGroup("Process Service Account");
   fs.writeFileSync(serviceAccountFile, serviceAccount);
-  try {
-    handleError(
-      childProcess.spawnSync(
-        `gcloud auth activate-service-account`,
-        [`--key-file=${serviceAccountFile}`],
-        childProcessOptions
-      )
-    );
-  } catch (err) {
-    throw new Error(err);
-  }
+  childProcess.execSync(
+    `gcloud auth activate-service-account --key-file ${serviceAccountFile}`,
+    childProcessOptions
+  );
   core.endGroup();
 }
 
 function setupGoogleCloudProject(projectId) {
   core.startGroup("Set Google Cloud Project");
-  try {
-    handleError(
-      childProcess.spawnSync(
-        `gcloud config set project`,
-        [`${projectId}`],
-        childProcessOptions
-      )
-    );
-  } catch (err) {
-    throw new Error(err);
-  }
+  childProcess.execSync(
+    `gcloud config set project ${projectId}`,
+    childProcessOptions
+  );
   core.endGroup();
 }
 
 function deployToGoogleCloudAppEngine(currentBranch) {
   core.startGroup("Deploy to Google Cloud App Engine");
-  try {
-    handleError(
-      childProcess.spawnSync(
-        "gcloud app deploy",
-        [
-          currentBranch.includes("develop") || currentBranch.includes("release")
-            ? "./app.staging.yaml"
-            : "./app.default.yaml",
-        ],
-        childProcessOptions
-      )
-    );
-  } catch (err) {
-    throw new Error(err);
-  }
+  childProcess.execSync(
+    `gcloud app deploy ./app.${
+      currentBranch.includes("develop") || currentBranch.includes("release")
+        ? "staging"
+        : "default"
+    }.yaml`,
+    childProcessOptions
+  );
   core.endGroup();
 }
 
@@ -76,5 +50,5 @@ try {
   unlinkServiceAccountFile(serviceAccountFile);
 } catch (error) {
   core.setFailed(error.message);
-  process.exit(1);
+  process.exit(0);
 }
