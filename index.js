aa9d6b093810221ce2315fb15283210644a35f90
Thomas Démocrite <thomas.democrite@pm.me>
2020-07-02 11:56:24 +0200

    feat: try catch

--- index.js
+++ index.js
@@ -6,39 +6,60 @@ const childProcessOptions = { stdio: "inherit" };
 
 function processServiceAccount(serviceAccount, serviceAccountFile) {
   core.startGroup("Process Service Account");
-  fs.writeFileSync(serviceAccountFile, serviceAccount);
-  childProcess.execSync(
-    `gcloud auth activate-service-account --key-file ${serviceAccountFile}`,
-    childProcessOptions
-  );
+  try {
+    fs.writeFileSync(serviceAccountFile, serviceAccount);
+    childProcess.execSync(
+      `gcloud auth activate-service-account --key-file ${serviceAccountFile}`,
+      childProcessOptions
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+    throw new Error(error);
+  }
+
   core.endGroup();
 }
 
 function setupGoogleCloudProject(projectId) {
   core.startGroup("Set Google Cloud Project");
-  childProcess.execSync(
-    `gcloud config set project ${projectId}`,
-    childProcessOptions
-  );
+  try {
+    childProcess.execSync(
+      `gcloud config set project ${projectId}`,
+      childProcessOptions
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+    throw new Error(error);
+  }
   core.endGroup();
 }
 
 function deployToGoogleCloudAppEngine(currentBranch) {
   core.startGroup("Deploy to Google Cloud App Engine");
-  childProcess.execSync(
-    `gcloud app deploy ./app.${
-      currentBranch.includes("develop") || currentBranch.includes("release")
-        ? "staging"
-        : "default"
-    }.yaml`,
-    childProcessOptions
-  );
+  try {
+    childProcess.execSync(
+      `gcloud app deploy ./app.${
+        currentBranch.includes("develop") || currentBranch.includes("release")
+          ? "staging"
+          : "default"
+      }.yaml`,
+      childProcessOptions
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+    throw new Error(error);
+  }
   core.endGroup();
 }
 
 function unlinkServiceAccountFile(serviceAccountFile) {
   core.startGroup("Unlink Service Account File");
-  fs.unlinkSync(serviceAccountFile);
+  try {
+    fs.unlinkSync(serviceAccountFile);
+  } catch (error) {
+    core.setFailed(error.message);
+    throw new Error(error);
+  }
   core.endGroup();
 }
 
@@ -50,5 +71,5 @@ try {
   unlinkServiceAccountFile(serviceAccountFile);
 } catch (error) {
   core.setFailed(error.message);
-  process.exit(0);
+  process.exit(1);
 }
